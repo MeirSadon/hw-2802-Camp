176c766f112d68f6f29c1e2ceef137597ad21b3f
MeirSadon <45739995+MeirSadon@users.noreply.github.com>
2019-03-03 01:08:30 +0200

    Add files via upload

---
+++ Camp.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Camp
+{
+    public class Camp
+    {
+        public readonly int id;
+        public int Latitude { get; set; }
+        public int Longitude {get; set;}
+        public int NumberOfPoeple { get; set; }
+        public int NumberOfTents { get; set; }
+        public int NumberOfFlashLights { get; set; }
+        private static int lastCampId = 0;
+
+        public Camp(int latitude, int longitude, int numberOfPoeple, int numberOfTents, int numberOfFlashLights)
+        {
+            this.id = lastCampId++;
+            Latitude = latitude;
+            Longitude = longitude;
+            NumberOfPoeple = numberOfPoeple;
+            NumberOfTents = numberOfTents;
+            NumberOfFlashLights = numberOfFlashLights;
+        }
+        public Camp()
+        {
+
+        }
+        public override string ToString()
+        {
+            return $"Camp Id: {id}. Latitude: {Latitude}. Longitude { Longitude}. Number Of People: {NumberOfPoeple}." +
+                $"Number Of Tents: {NumberOfTents}. Number Of Flash Lights: {NumberOfFlashLights}";
+        }
+        public static bool operator ==(Camp c1, Camp c2)
+        {
+            if (c1 == null || c2 == null)
+            {
+                Console.WriteLine("Not Found A Camp");
+                return false;
+            }
+            if (c1.id == c2.id)
+                return true;
+            return false;
+        }
+        public static bool operator !=(Camp c1, Camp c2)
+        {
+            if (c1 == null || c2 == null)
+            {
+                Console.WriteLine("Not Found A Camp");
+                return false;
+            }
+            return !(c1.id == c2.id);
+        }
+        public static bool operator >(Camp c1, Camp c2)
+        {
+            if (c1.NumberOfPoeple > c2.NumberOfPoeple)
+                return true;
+            return false;
+        }
+        public static bool operator <(Camp c1, Camp c2)
+        {
+            if (c1.NumberOfPoeple == c2.NumberOfPoeple)
+                return false;
+                return !(c1.NumberOfPoeple > c2.NumberOfPoeple);
+        }
+        public static Camp operator +(Camp c1, Camp c2)
+        {
+            Camp newCamp = new Camp(c1.Latitude + c2.Latitude, c1.Longitude + c2.Longitude, c1.NumberOfPoeple + c2.NumberOfPoeple,
+                c1.NumberOfTents + c2.NumberOfTents, c1.NumberOfFlashLights + c2.NumberOfFlashLights);
+            return newCamp;
+        }
+
+        public override bool Equals(object obj)
+        {
+            Camp c = obj as Camp;
+            if (this.id == c.id)
+                return true;
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.id;
+        }
+
+        public static void SerializeACamp(string fileName, Camp campFile)
+        {
+            XmlSerializer myXmlSerializer = new XmlSerializer(typeof(Camp));
+            using (Stream file = new FileStream(fileName, FileMode.Create))
+            {
+                myXmlSerializer.Serialize(file, campFile);
+            }
+        }
+
+        public static Camp DesrializerACamp(string fileName)
+        {
+            Camp newCamp = null;
+            XmlSerializer myXmlSerializer = new XmlSerializer(typeof(Camp));
+            using (Stream file = new FileStream(fileName, FileMode.Open))
+            {
+                newCamp = myXmlSerializer.Deserialize(file) as Camp;
+            }
+            return newCamp;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Camp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Camp[] campArray = new Camp[4];
+            campArray[0] = new Camp(50, 15, 4, 3, 5);
+            campArray[1] = new Camp(50, 5, 6, 7, 5);
+            campArray[2] = new Camp(50, 15, 4, 3, 5);
+            campArray[3] = new Camp(50, 15, 4, 3, 5);
+
+
+            foreach (Camp camp in campArray)
+            {
+                Console.WriteLine(camp);
+            }
+
+
+            if (campArray[0] > campArray[1])
+                Console.WriteLine("c1 Bigger Than c2");
+            Camp newCamp = campArray[0] + campArray[1];
+            Console.WriteLine(newCamp);
+
+            string path = "C:/temp/Camp.Xml";
+            Camp.SerializeACamp(path, campArray[0]);
+            Camp newCamp1 = Camp.DesrializerACamp(path);
+            Camp newCamp2 = Camp.DesrializerACamp(path);
+
+            if (ReferenceEquals(newCamp1, newCamp2))
+                    Console.WriteLine(newCamp1 + newCamp2);
+                else
+                    Console.WriteLine("BBOOMMM");
+
+            int a = newCamp1.GetHashCode();
+            int b = newCamp2.GetHashCode();
+            if (a == b)
+                Console.WriteLine(a+b);
+            else
+                Console.WriteLine("NOT");
+            }
+        }
+    }
